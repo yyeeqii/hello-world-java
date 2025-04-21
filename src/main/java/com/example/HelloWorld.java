e79a4de0c92abaffa3fe65f5926f1b3b34cd8009
AndySee04 <andystkei@gmail.com>
2025-04-21 16:18:20 +0800

    added another language

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Ohayo, sekai!");
+        System.out.println("Bonjour, le monde!");
     }
 
     public String getGreeting() {
